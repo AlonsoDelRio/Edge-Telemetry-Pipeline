32b4af81189de687cffdbf293f5793e669bd420e
Omar Alonso Del Rio P. <alonso.delrio@uabc.edu.mx>
2026-04-10 12:40:46 -0700

    refactor(telemetry): update mqtt topic structure for hardware and simulation

--- firmware/src/config/config_hw.h
+++ firmware/src/config/config_hw.h
@@ -3,10 +3,10 @@
 #include "secrets.h"
 
 // MQTT Suscribe topics
-static const char* MQTT_SUB_WATERPUMP = "edge-pipeline/ESP32/waterpump";
+static const char* MQTT_SUB_WATERPUMP = "edge-pipeline/hw/esp32-01/waterpump/set";
 
 // MQTT publish topics
-static const char* MQTT_PUB_DHT_TEMP  = "edge-pipeline/ESP32/dht/temp";
-static const char* MQTT_PUB_DHT_HUM   = "edge-pipeline/ESP32/dht/hum";
-static const char* MQTT_PUB_SOIL_HUM   = "edge-pipeline/ESP32/soil/moisture";
-static const char* MQTT_PUB_LIGHT_LEVEL   = "edge-pipeline/ESP32/light/illuminance";
+static const char* MQTT_PUB_DHT_TEMP  = "edge-pipeline/hw/esp32-01/dht/temp";
+static const char* MQTT_PUB_DHT_HUM   = "edge-pipeline/hw/esp32-01/dht/hum";
+static const char* MQTT_PUB_SOIL_HUM   = "edge-pipeline/hw/esp32-01/soil/moisture";
+static const char* MQTT_PUB_LIGHT_LEVEL   = "edge-pipeline/hw/esp32-01/light/illuminance";
--- firmware/src/config/config_sim.h
+++ firmware/src/config/config_sim.h
@@ -10,10 +10,10 @@
 #define MQTT_PORT 1883
 
 // MQTT Suscribe topics
-static const char* MQTT_SUB_WATERPUMP = "sipa/edge-pipeline/sym/waterpump";
+static const char* MQTT_SUB_WATERPUMP = "sipa/edge-pipeline/sim/esp32-00/waterpump/set";
 
 // MQTT publish topics
-static const char* MQTT_PUB_DHT_TEMP  = "sipa/edge-pipeline/sym/dht/temp";
-static const char* MQTT_PUB_DHT_HUM   = "sipa/edge-pipeline/sym/dht/hum";
-static const char* MQTT_PUB_SOIL_HUM   = "sipa/edge-pipeline/sym/soil/moisture";
-static const char* MQTT_PUB_LIGHT_LEVEL   = "sipa/edge-pipeline/sym/light/illuminance";
+static const char* MQTT_PUB_DHT_TEMP  = "sipa/edge-pipeline/sim/esp32-00/dht/temp";
+static const char* MQTT_PUB_DHT_HUM   = "sipa/edge-pipeline/sim/esp32-00/dht/hum";
+static const char* MQTT_PUB_SOIL_HUM   = "sipa/edge-pipeline/sim/esp32-00/soil/moisture";
+static const char* MQTT_PUB_LIGHT_LEVEL   = "sipa/edge-pipeline/sim/esp32-00/light/illuminance";
