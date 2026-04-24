785cec2ff707b69b3c1e7314145c2aeb81bd5908
Omar Alonso Del Rio P. <alonso.delrio@uabc.edu.mx>
2026-04-23 23:43:24 -0700

    fix(db): quote variables in setup-buckets.sh to prevent word splitting
    Address ShellCheck warning SC2086 by wrapping bucket and organization variables in double quotes. This ensures robust execution during automated CI provisioning, preventing potential runtime failures if environment variables ever contain unexpected whitespace.

--- docker/influxdb/setup-buckets.sh
+++ docker/influxdb/setup-buckets.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-influx bucket create -n ${INFLUXDB_BUCKET_SIM} -o ${INFLUXDB_ORG} -r 24h
-influx bucket create -n ${INFLUXDB_BUCKET_HW} -o ${INFLUXDB_ORG} -r 0 
+influx bucket create -n "${INFLUXDB_BUCKET_SIM}" -o "${INFLUXDB_ORG}" -r 24h
+influx bucket create -n "${INFLUXDB_BUCKET_HW}" -o "${INFLUXDB_ORG}" -r 0
